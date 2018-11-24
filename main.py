b94f17a42e3869eea1f16c326c2bbec4a79e2459
Pranav Bhatia <bhatiapranav1996@gmail.com>
2018-11-24 10:37:27 -0500

    splitting data into test and training set

--- main.py
+++ main.py
@@ -4,9 +4,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.utils import shuffle
 
 # Import the dataset
 dataset = pd.read_csv('heart.csv')
+dataset = shuffle(dataset)
 X = dataset.iloc[:, :-1].values #matrix of independent variables
 Y = dataset.iloc[:, 13].values #matrix of dependent variables
 
@@ -15,3 +17,7 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'most_frequent', axis = 0)
 imputer.fit(X)
 X = imputer.transform(X)
+
+# Splitting the dataset into training and test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
