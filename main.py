dafcaa1e73fbb2005117bc1e660c2eca0dc55585
Pranav Bhatia <bhatiapranav1996@gmail.com>
2018-11-24 06:39:48 -0500

    missing data handled

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ import pandas as pd
 dataset = pd.read_csv('heart.csv')
 X = dataset.iloc[:, :-1].values #matrix of independent variables
 Y = dataset.iloc[:, 13].values #matrix of dependent variables
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'most_frequent', axis = 0)
+imputer.fit(X)
